7566bea90c756af0aa8266cf13aa7be1c6ff694a
HAMPERHAMPS <149680144+HAMPERHAMPS@users.noreply.github.com>
2024-03-07 10:31:41 -0700

    Add files via upload

---
+++ ggs.py
@@ -0,0 +1,27 @@
+
+import os
+from slack_sdk import WebClient
+from flask import Flask, request, jsonify
+from slack_bolt import App
+from slack_sdk.web import WebClient
+import time
+
+
+
+SLACK_BOT_TOKEN = os.environ.get("xoxe.xoxp-1-Mi0yLTY3NDE2OTgyNzMyODMtNjc0MTczNjI5ODE5NS02Nzc5NzQyMzQyMDQ4LTY3NDIzOTYwNzk5OTEtMDVjNjAzYTg3ZmMxODEwZDkyNTNlOTg3YjVhZmNmMjc1NDdkNjI3Mjc2ZDQ4YTE0MjczYjYyNDI3NGUwNGQyNQ")
+client = WebClient(token="xoxb-6741698273283-6751165525062-bSOSqx7v8HqoOM27iyNFH6GK")
+
+
+sc = WebClient("6741698273283.6751134445030")
+
+channel_id = "C06MQS1FN3X"
+
+
+client.chat_postMessage(channel="C06MQS1FN3X", text="Heheh i bobby (This is an automated startup message if it starts spamming this tell Mr.Hamps)")
+nig=1
+cunt = 1
+while True:
+    client.chat_postMessage(channel="C06MQS1FN3X", text="New gc without slackbot will be made soon")
+    time.sleep(1000)
+    
+ 
